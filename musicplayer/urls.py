4fac63e1bf772f3943f35d6199b0e912be7050e1
Karun <karuun0196@gmail.com>
2023-03-08 13:57:16 +0545

    Finally

--- musicplayer/urls.py
+++ musicplayer/urls.py
@@ -5,6 +5,8 @@ from musicplayer import views
 
 urlpatterns = [
     path('songs', views.songs, name='songs'),
-    path('songs/<int:id>', views.songpost, name='songpost')
+    path('songs/<int:id>', views.songpost, name='songpost'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup')
 
 ]
--- musicplayer/views.py
+++ musicplayer/views.py
@@ -1,16 +1,47 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from musicplayer.models import Songs
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
 def index(request):
-    song= Songs.objects.all()
-    return render(request,'index.html',{'song':song})
+    song = Songs.objects.all()
+    return render(request, 'index.html', {'song': song})
+
 
 def songs(request):
-    song= Songs.objects.all()
-    return render(request,'songs.html',{'song':song})
+    song = Songs.objects.all()
+    return render(request, 'songs.html', {'song': song})
 
 
-def songpost(request,id):
-    song= Songs.objects.filter(song_id=id).first()
-    return render(request,'songpost.html',{'song':song})
+def songpost(request, id):
+    song = Songs.objects.filter(song_id=id).first()
+    return render(request, 'songpost.html', {'song': song})
+
+
+def login(request):
+    if request.method == "POST":
+      username=request.POST['username']
+      password=request.POST['pass']
+     
+      user= authenticate(Username=username, Password=password)
+      login(request,user)
+      redirect("/")
+    return render(request,'login.html')
+
+def signup(request):
+     if request.method=="POST":
+      username=request.POST['username']
+      pass1=request.POST['pass1']
+      pass2=request.POST['pass2']
+
+      myuser=User.objects.create_user(username,pass1)
+      myuser.save()
+      user= authenticate(Username=username, Password=pass1)
+      login(request,user)
+
+      return redirect('/')
+     return render(request,'signup.html')
